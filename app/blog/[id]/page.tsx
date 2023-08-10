715c2208dce438f07a2dd68157468bd39a4c88d2
ilhombek2110 <ilhombek2110@gmail.com>
2023-08-10 10:50:28 +0500

    first commit

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,8 @@
+type Props = {
+  params: {
+    id: string;
+  };
+};
+export default function Post({ params: { id } }: Props) {
+  return <h1>Post page {id}</h1>;
+}
---
+++ components/TheFooter.tsx
@@ -0,0 +1,5 @@
+const TheFooter = () => {
+  return <footer className="">Created by &copy;NEXTJS NEW ILHOMBEK</footer>;
+};
+
+export { TheFooter };
